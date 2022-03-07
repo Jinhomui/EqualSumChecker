4882452bca9be6d33cf3e04d4bda644ae55f6c1a
Christian Mui <jinhomui27@gmail.com>
2022-03-06 21:25:36 -0800

    EqualSumChecker

---
+++ src/com/company/EqualSumChecker.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class EqualSumChecker {
+    public static boolean hasEqualSum(int no1, int no2, int no3){
+        if(no1 + no2 == no3){
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
+
